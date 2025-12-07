eae0cf2c600363485bfd63353a3c8cc086c6d564
NimbusPaw <nimbuspaw@gmail.com>
2025-12-08 01:01:33 +0200

    in progress

--- packages/fields/src/field-tree.ts
+++ packages/fields/src/field-tree.ts
@@ -17,9 +17,6 @@ class DefaultNodeFactory implements TreeNodeFactory {
 /** A type alias for any container that can be a child node in a FieldTree */
 export type TreeOrFieldsNode = FieldTree | Fields;
 
-/** A helper type representing the constructor of a container */
-// type ContainerCtor<T extends TreeOrFieldsContainer> = new () => T;
-
 /** Describes the payload for events emitted when a container is created or removed from a FieldTree. */
 // export type FieldTreeContainerEvent = {
 //   type: 'created' | 'removed';
@@ -44,7 +41,7 @@ export class FieldTree {
   private readonly _items: Map<string, TreeOrFieldsNode> = new Map();
   private readonly _factory: TreeNodeFactory;
 
-//   // readonly events = new AxiEventEmitter<'created' | 'removed'>();
+  // readonly events = new AxiEventEmitter<'created' | 'removed'>();
 
   get items() {
     return this._items;
@@ -127,92 +124,45 @@ export class FieldTree {
 
   /**
    * Retrieves a child node and asserts that it is an instance of `FieldTree`.
-   * @param name The name of the child node.
+   * @param path The name or path to the child node.
    * @returns The `FieldTree` instance.
    * @throws If the node does not exist or is not a `FieldTree`.
    */
-  getFieldTree(name: string) {
-    const node = this.getNode(name);
-    throwIf(!(node instanceof FieldTree), `Node '${name}' should be instance of FieldTree`);
-    return node as FieldTree;
+  getFieldTree(path: PathType): FieldTree {
+    const pathArr = ensurePathArray(path);
+    throwIfEmpty(pathArr, 'The path is empty');
+    let container: FieldTree | Fields = this;
+    for (let i = 0; i < pathArr.length; i++) {
+      throwIf(!(container instanceof FieldTree), `Node '${pathArr[0]}' should be instance of FieldTree`);
+      container = (container as FieldTree).getNode(pathArr[i]);
+    }
+    return container as FieldTree;
   }
 
   /**
    * Retrieves a child node and asserts that it is an instance of `Fields`.
-   * @param name The name of the child node.
+   * @param path The name of the child node.
    * @returns The `Fields` instance.
    * @throws If the node does not exist or is not a `Fields` container.
    */
-  getFields(name: string) {
-    const node = this.getNode(name);
-    throwIf(!(node instanceof Fields), `Node '${name}' should be instance of Fields`);
+  getFields(path: PathType) {
+    const pathArr = ensurePathArray(path);
+    throwIfEmpty(pathArr, 'The path is empty');
+    const name = pathArr.pop()!;
+
+    const tree = !pathArr.length ? this : this.getFieldTree(pathArr);
+    const node = tree.getNode(name);
+
+    throwIf(!(node instanceof Fields), `Node '${ensurePathString(path)}' should be instance of Fields`);
     return node as Fields;
   }
 
-  /**
-   * Navigates through the tree using a path and returns the `Fields` container at the end.
-   * @param path The path to the `Fields` container (e.g., 'player/stats').
-   * @returns The `Fields` container at the specified path.
-   * @throws If the path is empty, or any intermediate node is not a `FieldTree`.
-   */
-  getFieldsByPath(path: PathType) {
-    const pathParts = ensurePathArray(path);
-    throwIf(!pathParts.length, 'Empty path');
-    let container: FieldTree = this;
-    for (let i = 0; i < pathParts.length - 1; i++) {
-      container = container.getFieldTree(pathParts[i]);
-    }
-    return container.getFields(pathParts[pathParts.length - 1]);
+  private traversePath(
+    path: PathType,
+    options: { createPath?: boolean } = {}
+  ) {
+
   }
-
-//   /**
-//    * Creates a `Field` at a deeply nested path.
-//    * The last part of the path is treated as the field name, and the preceding parts as the path to its container.
-//    * @param path The full path to the new field (e.g., 'player/stats/health').
-//    * @param initialValue The initial value for the new field.
-//    * @returns The newly created `Field` instance.
-//    */
-//   create<T>(path: PathType, initialValue: T): BaseField<T> {
-//     const fullPath = [...ensurePathArray(path)];
-//     const fieldName = fullPath.pop()!;
-//     throwIf(!fullPath.length, `Wrong path format of one field creating: '${ensurePathString(path)}', should be at least two sections`);
-//     return this.getFieldsByPath(fullPath).create<T>(fieldName, initialValue);
-//   }
-//
-//   /**
-//    * Creates a `NumberField` at a deeply nested path.
-//    * @param path The full path to the new field (e.g., 'player/stats/mana').
-//    * @param initialValue The initial numeric value.
-//    * @returns The newly created `NumberField` instance.
-//    */
-//   createNumber(path: PathType, initialValue: number): NumberField {
-//     const fullPath = [...ensurePathArray(path)];
-//     const fieldName = fullPath.pop()!;
-//     return this.getFieldsByPath(fullPath).createNumber(fieldName, initialValue);
-//   }
-//
-//   /**
-//    * Retrieves a `Field` from a deeply nested path.
-//    * @param path The full path to the field (e.g., 'player/stats/name').
-//    * @returns The `Field` instance at the specified path.
-//    */
-//   get<T>(path: PathType): BaseField<T> {
-//     const fullPath = [...ensurePathArray(path)];
-//     const fieldName = fullPath.pop()!;
-//     return this.getFieldsByPath(fullPath).get<T>(fieldName);
-//   }
-//
-//   /**
-//    * Retrieves a `NumberField` from a deeply nested path.
-//    * @param path The full path to the number field (e.g., 'player/stats/level').
-//    * @returns The `NumberField` instance at the specified path.
-//    */
-//   getNumber(path: PathType): NumberField {
-//     const fullPath = [...ensurePathArray(path)];
-//     const fieldName = fullPath.pop()!;
-//     return this.getFieldsByPath(fullPath).getNumber(fieldName);
-//   }
-
 
 //   /**
 //    * Creates a serializable snapshot of the entire tree and its contained fields.
@@ -252,29 +202,5 @@ export class FieldTree {
 //       }
 //       node?.hydrate(field);
 //     }
-//   }
-//
-//   /**
-//    * @private
-//    * Generic internal method for creating and adding a new node to the tree.
-//    * @param name The name of the node to create.
-//    * @param ctor The constructor for the node type (e.g., `FieldTree` or `Fields`).
-//    * @returns The newly created node instance.
-//    */
-//   private createNode<T extends TreeOrFieldsContainer>(name: string, ctor: ContainerCtor<T>): T {
-//     throwIf(this._items.has(name), `Can't create node with name: '${name}', node already exists`);
-//
-//     const res = new ctor();
-//     this._items.set(name, res);
-//
-//     /** todo: restore */
-//     // this.events.emit('created', {
-//     //   type: 'created',
-//     //   name: name,
-//     //   path: [], // todo: need to decide how to pass full path
-//     //   node: res
-//     // } as FieldTreeContainerEvent);
-//
-//     return res;
 //   }
 }
