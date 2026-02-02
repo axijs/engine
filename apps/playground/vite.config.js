92853af6b8f239b7c00fad056ad43aafb5cd939d
NimbusPaw <nimbuspaw@gmail.com>
2026-02-03 00:59:55 +0200

    small improvement in the playground vite config

--- apps/playground/vite.config.js
+++ apps/playground/vite.config.js
@@ -4,9 +4,11 @@ import path from 'path';
 export default defineConfig({
   resolve: {
     alias: {
-      '@axi-engine/utils': path.resolve(__dirname, '../../packages/utils/src'),
       '@axi-engine/fields': path.resolve(__dirname, '../../packages/fields/src'),
       '@axi-engine/states': path.resolve(__dirname, '../../packages/states/src'),
+      '@axi-engine/tasks': path.resolve(__dirname, '../../packages/tasks/src'),
+      '@axi-engine/tasks-anime': path.resolve(__dirname, '../../packages/tasks-anime/src'),
+      '@axi-engine/utils': path.resolve(__dirname, '../../packages/utils/src'),
     },
   },
 
@@ -20,9 +22,11 @@ export default defineConfig({
   // exclude packages catching
   optimizeDeps: {
     exclude: [
-      '@axi-engine/utils',
       '@axi-engine/fields',
-      '@axi-engine/states'
+      '@axi-engine/states',
+      '@axi-engine/tasks',
+      '@axi-engine/tasks-anime',
+      '@axi-engine/utils'
     ],
   }
 })
