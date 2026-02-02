b81a9eea28a37d3e49d442cbefa298e76a11ea80
NimbusPaw <nimbuspaw@gmail.com>
2026-02-02 20:04:01 +0200

    testing Tasks.sequence

--- apps/playground/src/test-tasks.ts
+++ apps/playground/src/test-tasks.ts
@@ -14,7 +14,7 @@ export async function testTasks() {
   const sequenceTask2 = Tasks.sequence<number | void>([
     Tasks.wait(100),
     Tasks.wait(200),
-    Tasks.sync<number | void>(()  => {
+    Tasks.sync<number | void>(() => {
       if (randInt(0, 2) === 1) {
         return 100
       }
@@ -23,4 +23,27 @@ export async function testTasks() {
 
   const seq2res = await sequenceTask2.promise;
   console.log('sequence 2 res: ', seq2res);
+
+  const controllable1 = Tasks.controllable();
+  const controllable2 = Tasks.controllable();
+
+  setTimeout(() => {
+    console.log('controllable1: resolve after 1sec')
+    controllable1.controller.resolve(undefined)
+  }, 1000);
+
+  setTimeout(() => {
+    console.log('controllable2: resolve after 2sec')
+    controllable2.controller.resolve(undefined)
+  }, 2000);
+
+  const sequenceTask3 = Tasks.sequence<void>([
+    controllable1.task,
+    Tasks.sync(() => {
+      console.log('Tasks.sync')
+    }),
+    controllable2.task
+  ]);
+
+  await sequenceTask3.promise;
 }
