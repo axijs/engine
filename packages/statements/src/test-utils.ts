7945f3befbfeecbeb492c784a902282f3c41bd8a
NimbusPaw <nimbuspaw@gmail.com>
2026-02-18 00:46:02 +0200

    unit tests for statements module in progress

--- packages/statements/src/test-utils.ts
+++ packages/statements/src/test-utils.ts
@@ -2,7 +2,33 @@
 
 import { vi } from 'vitest';
 import { StatementResolverContext } from './statement-resolver-context';
+import {DataStorage, PathType} from '@axi-engine/utils';
+import {ExpressionEvaluator} from '@axi-engine/expressions';
+import {StatementResolver} from './statement-resolver';
 
-export function createMockContext() {
-
-}
+// export function createMockContext(): StatementResolverContext {
+//   const storage: DataStorage = {
+//     get: vi.fn(),
+//     set: vi.fn(),
+//     has: vi.fn(),
+//     delete: vi.fn(),
+//     create: vi.fn(),
+//     upset: vi.fn(),
+//   };
+//
+//   const expressions = {
+//     resolve: vi.fn(),
+//     register: vi.fn(),
+//   };
+//
+//   const statements = {
+//     execute: vi.fn(),
+//   };
+//
+//   // const ctx: StatementResolverContext = {
+//   //   storage(): DataStorage { return storage; },
+//   //   expressions(): ExpressionEvaluator { return expressions; },
+//   //   statements(): StatementResolver {
+//   //   },
+//   // };
+// }
