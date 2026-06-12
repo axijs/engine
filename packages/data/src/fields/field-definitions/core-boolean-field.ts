9700c2b28ff4dee93ad383aeead2d91cc34b971a
NimbusPaw <nimbuspaw@gmail.com>
2026-06-12 16:14:55 +0300

    in progress

--- packages/data/src/fields/field-definitions/core-boolean-field.ts
+++ packages/data/src/fields/field-definitions/core-boolean-field.ts
@@ -4,8 +4,8 @@ import {BooleanField, FieldOptions} from '../field';
 export interface CoreBooleanFieldOptions extends FieldOptions<boolean> { }
 
 export class CoreBooleanField extends CoreField<boolean> implements BooleanField {
-  static readonly typeName: string = 'boolean';
-  readonly typeName = CoreBooleanField.typeName;
+  static override readonly typeName: string = 'boolean';
+  override readonly typeName = CoreBooleanField.typeName;
 
   constructor(name: string, initialVal: boolean, options?: CoreBooleanFieldOptions) {
     super(name, initialVal, options);
